8f3413cc301e54deefe8a5050d924c37a835fdeb
Kenneth Chen <kennethchendbgt@gmail.com>
2016-12-21 15:11:22 -0500

    Install ChemDoodleWeb

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const BattleshipGame = require('./game.js');
 const BattleshipView = require('./battleship_view.js');
 
 $( () => {
-  const rootEl = $('.ptbattleship');
+  const rootEl = $('.pt-battleship');
   const game = new BattleshipGame();
   new BattleshipView(game, rootEl);
 });
